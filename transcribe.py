c25d54ef02e6ef9b5a2570ac26b682d5d6950ab5
James Vodanovich <james@jvodan.com>
2023-05-04 13:22:12 +1000

    add model path option

--- transcribe.py
+++ transcribe.py
@@ -95,6 +95,7 @@ def main():
     parser.add_argument("-M", "--model_name", help="The name of the Whisper model to use. tiny|base|small|medium")
     parser.add_argument("-f", "--fp16", action="store_true", help="Enable mixed-precision inference. 16 not always faster 32 better on maxwell")
     parser.add_argument("-w", "--wav-path", help="The path to the input directory of WAVs to transcribe.")
+    parser.add_argument("-p", "--model-path", help="The path to the directory containing the models. Leave blank for ~/.cache/whisper")
     parser.add_argument("-o", "--output-path", help="The directory path to write the output transcript files.")
     parser.add_argument("-p", "--pipe-path", help="The path to the named pipe to write the output transcript to.")
     parser.add_argument("-W", "--watch", action="store_true", help="Watch the wav_path and transcbribe files as they appear.")
